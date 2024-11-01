0152cfe3053ba36d4633defaecc3060a5d29aaf2
unit-404 <conway24b@gmail.com>
2024-11-01 23:50:53 +0700

    planned new experimental project about UI components

---
+++ vite.config.js
@@ -0,0 +1,64 @@
+import rollupOptions, {plugins, NAME} from "./rollup/rollup.config";
+import {resolve} from "node:path";
+
+//
+import cssnano from "cssnano";
+import deduplicate from "postcss-discard-duplicates";
+import postcssPresetEnv from 'postcss-preset-env';
+import autoprefixer from "autoprefixer";
+
+//
+export const __dirname = resolve(import.meta.dirname, "./");
+export default {
+    plugins,
+    server: {
+        port: 5173,
+        open: false,
+        origin: "http://localhost:5173",
+    },
+    build: {
+        chunkSizeWarningLimit: 1600,
+        assetsInlineLimit: 1024 * 1024,
+        minify: "terser",
+        sourcemap: 'hidden',
+        target: "esnext",
+        name: NAME,
+        lib: {
+            formats: ["es"],
+            entry: resolve(__dirname, './src/main/index.ts'),
+            name: NAME,
+            fileName: NAME,
+        },
+        rollupOptions
+    },
+    optimizeDeps: {
+        include: [
+            "./node_modules/**/*.mjs",
+            "./node_modules/**/*.js",
+            "./node_modules/**/*.ts",
+            "./src/**/*.mjs",
+            "./src/**/*.js",
+            "./src/**/*.ts",
+            "./src/*.mjs",
+            "./src/*.js",
+            "./src/*.ts",
+            "./test/*.mjs",
+            "./test/*.js",
+            "./test/*.ts"
+        ],
+        entries: [resolve(__dirname, './src/index.ts'),],
+        force: true
+    },
+    css: {
+        postcss: {
+            plugins: [autoprefixer(), deduplicate(), cssnano({
+                preset: ['advanced', {
+                    calc: false,
+                    discardComments: {
+                        removeAll: true
+                    }
+                }],
+            }), postcssPresetEnv({ stage: 0 })],
+        },
+    },
+};
