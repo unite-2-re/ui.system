14617e9fe84ffefeb0e7a0c496cf4833d7126394
u2re-dev <conway24b@yandex.ru>
2025-01-02 10:43:22 +0700

    reduce code

--- src/$design$/index.ts
+++ src/$design$/index.ts
@@ -1,3 +1,16 @@
-export * from "./$core$/Loader";
-import init from "./$core$/Loader";
-export default init;
+// @ts-ignore
+import { hash, loadInlineStyle } from "/externals/lib/dom.js";
+
+// @ts-ignore
+import styles from "./$scss$/Bundle.scss?inline&compress";
+const preInit = URL.createObjectURL(new Blob([styles], {type: "text/css"}));
+const integrity = hash(styles);
+export const styleCode = {preInit, integrity, styles};
+
+//
+const initialize = (rootElement = document.head)=>{
+    loadInlineStyle(preInit, rootElement);
+}
+
+//
+export default initialize;
