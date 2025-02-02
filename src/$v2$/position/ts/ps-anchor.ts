d3d30e68106a60c783a0cc99640d007bc7247472
u2re-dev <c24b@u2re.ru>
2025-02-02 20:29:15 +0700

    attempt to fix position

--- src/$v2$/position/ts/ps-anchor.ts
+++ src/$v2$/position/ts/ps-anchor.ts
@@ -14,31 +14,35 @@ export const placeWithElement = (self?: HTMLElement, element?: HTMLElement, wher
         element && element.dataset?.hidden == null &&
         self && self?.dataset?.hidden == null
     ) {
-        const box = getBoundingOrientRect(element);
+        const box      = getBoundingOrientRect(element);
         const self_box = getBoundingOrientRect(self);
+        const anchor   = element?.style?.getPropertyValue?.("anchor-name");
 
         //
-        const initialAnchor = element?.style?.getPropertyValue?.("anchor-name");
-        const ID = generateId();
-        if (!initialAnchor || initialAnchor == "none") {
-            element?.style?.setProperty?.("anchor-name", "--" + ID, "");
+        let ID = "--" + generateId();
+        if (!anchor || anchor == "none") {
+            element?.style?.setProperty?.("anchor-name", ID, "");
+        } else {
+            ID = anchor;
         }
 
         //
-        self.style.setProperty("--anchor-group", (element?.style?.getPropertyValue?.("anchor-name") || ("--" + ID)), "");
+        self.style.setProperty("--anchor-group", ID, "");
         self.style.setProperty("--inline-size", (box.width || self_box.width || 0) + "px", "");
 
         //
-        //requestAnimationFrame(()=>{
-            const updated_box = getBoundingOrientRect(self);
-            self.style.setProperty("--client-x", `${(box.left || 0) + (box.width - updated_box.width) * 0.5}`);
+        const updated_box = getBoundingOrientRect(self), style = getComputedStyle(self);
+        const min_height  = Math.max(parseFloat(style?.minBlockSize  || "0") || 0, parseFloat(style?.blockSize  || "0") || 0);
+        const min_width   = Math.max(parseFloat(style?.minInlineSize || "0") || 0, parseFloat(style?.inlineSize || "0") || 0);
 
-            // for taskbar/navbar
-            if (where == "from-top") {
-                self.style.setProperty("--client-y", `${(box.top - updated_box.height - gap)}`);
-            } else {
-                self.style.setProperty("--client-y", `${((box.bottom + gap) || 0)}`);
-            }
-        //});
+        //
+        self.style.setProperty("--client-x", `${(box.left || 0) + (box.width - Math.max(updated_box.width, min_width)) * 0.5}`);
+
+        // for taskbar/navbar
+        if (where == "from-top") {
+            self.style.setProperty("--client-y", `${(box.top - Math.max(updated_box.height, min_height) - gap)}`);
+        } else {
+            self.style.setProperty("--client-y", `${((box.bottom + gap) || 0)}`);
+        }
     }
 }
